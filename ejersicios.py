5970bd50ecad17b72098fea208ab2b0645518c0c
alexander muñoz <alexander12345my@gmail.com>
2023-05-05 08:58:18 -0500

    cambio de divisas

--- ejersicios.py
+++ ejersicios.py
@@ -3,29 +3,47 @@ ejersicios con python
 calculadora 
 """
 
-#calculadora
 
-# def Calculadora():
-num1 = input("Ingrese el primer número: ")
-ope = input("Ingrese la operación (+, -, *, /): ")
-num2 = input("Ingrese el segundo número: ")
-num1 = float(num1)
-num2 = float(num2)
-if ope == "+":
-    resultado = num1 + num2
-elif ope == "-":
-    resultado = num1 - num2
-elif ope == "*":
-    resultado = num1 * num2
-elif ope == "/":
-    resultado = num1 / num2
+# num1 = input("Ingrese el primer número: ")
+# ope = input("Ingrese la operación  [+, -, *, /]: ")
+# num2 = input("Ingrese el segundo número: ")
+# num1 = float(num1)
+# num2 = float(num2)
+# if ope == "+":
+#     resultado = num1 + num2
+# elif ope == "-":
+#     resultado = num1 - num2
+# elif ope == "*":
+#     resultado = num1 * num2
+# elif ope == "/":
+#     resultado = num1 / num2
+# else:
+#     print("Operación no válida.")
+
+# print("El resultado es: ", int(resultado))
+
+
+
+# cambio de divisas 
+
+pesos = input('ingrese su valor en pesos: ')
+cambio = input('escoja la moneda de cambio ["Dolares", Euros", "Libras"]: ')
+
+pesos = float(pesos)
+cambio = cambio.lower()
+
+if cambio == "dolares":
+    resultado = pesos * 0.00022
+elif cambio == "euros":
+    resultado = pesos * 0.00019
+elif cambio == "libras":
+    resultado = pesos * 0.00016
 else:
-    print("Operación no válida.")
+    print('operacion invalida')
 
-print("El resultado es: ", int(resultado))
+print('resultado: ', resultado , cambio)
 
 
-# Calculadora()
 
 
 
