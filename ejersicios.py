bf3a7ee2195863dfe1dc671aeaeb038ae1895e8d
alexander muñoz <alexander12345my@gmail.com>
2023-05-09 18:59:48 -0500

    ejersicio de idiomas

--- ejersicios.py
+++ ejersicios.py
@@ -26,22 +26,46 @@ calculadora
 
 # cambio de divisas 
 
-pesos = input('ingrese su valor en pesos: ')
-cambio = input('escoja la moneda de cambio ["Dolares", Euros", "Libras"]: ')
+# pesos = input('ingrese su valor en pesos: ')
+# cambio = input('escoja la moneda de cambio ["Dolares", Euros", "Libras"]: ')
 
-pesos = float(pesos)
-cambio = cambio.lower()
+# pesos = float(pesos)
+# cambio = cambio.lower()
 
-if cambio == "dolares":
-    resultado = pesos * 0.00022
-elif cambio == "euros":
-    resultado = pesos * 0.00019
-elif cambio == "libras":
-    resultado = pesos * 0.00016
-else:
-    print('operacion invalida')
+# if cambio == "dolares":
+#     resultado = pesos * 0.00022
+# elif cambio == "euros":
+#     resultado = pesos * 0.00019
+# elif cambio == "libras":
+#     resultado = pesos * 0.00016
 
-print('resultado: ', int(resultado) , cambio)
+# else:
+#     print('operacion invalida')
+
+# print('resultado: ', int(resultado) , cambio)
+
+
+#-------------------------------------------------
+
+# nombre = input('cual es tu nombre: ')
+# idioma = input('que idioma hablas [Español, Ingles, Portugues, Chino-Mandarin]: ')
+
+# idioma = idioma.lower()
+
+# if idioma == 'chino':
+#     print(f'{nombre} 你他媽的是個中國人')
+
+# elif idioma == 'español':
+#     print(f'{nombre} eres un latino de mierda(pobre)')
+
+# elif idioma == 'ingles':
+#     print(f'{nombre} you are a gringo of shit but you have money)')
+
+# elif idioma == 'portugues':
+#     print(f'{nombre} voce e um latino')
+
+# else:
+#     print('dato incorrecto')
 
 
 
